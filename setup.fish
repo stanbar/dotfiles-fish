5c6ed40d5b15505e8809519ced975bfe8e6e5cff
Stanislaw Baranski <stasbar1995@gmail.com>
2019-02-19 01:33:03 +0100

    Update setup.fish

--- setup.fish
+++ setup.fish
@@ -19,30 +19,34 @@ sudo apt install -y openssl
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
 sudo apt install -y unzip
-sudo apt install -y fish
+sudo apt autoremove -y
 
+# install fish
+sudo apt install -y fish
 chsh -s /usr/bin/fish
-curl https://getmic.ro | bash
-sudo mv micro /usr/bin/micro
-sudo apt autoremove
 
 # install dotfiles 
-
-git clone https://github.com/stasbar/dotfiles-fish.git ~/dotfiles/fish
 touch ~/dotfiles/fish/secrets.fish
+fish &
 echo 'source ~/dotfiles/fish/profile.fish' >> ~/.config/fish/config.fish
 ln -s ~/dotfiles/fish/functions ~/.config/fish/functions
 
+# install micro
+curl https://getmic.ro | bash
+sudo mv micro /usr/bin/micro
 
 # download and install android sdk
 wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
 sudo unzip sdk-tools-linux-4333796.zip
+mkdir android-sdk-linux
 sudo mv tools android-sdk-linux
+rm sdk-tools-linux-4333796.zip
 sudo chown -R stasbar1995:stasbar1995 android-sdk-linux/
-
-sdkmanager --update
-sdkmanager --install
-sdkmanager --licenses
+cd android-sdk-linux
+tools/bin/sdkmanager --update
+tools/bin/sdkmanager --install
+tools/bin/sdkmanager --licenses
+cd ~
 
 # add directories
 mkdir ~/.keys
@@ -51,5 +55,5 @@ mkdir ~/projects
 # install keybase
 curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb
 sudo dpkg -i keybase_amd64.deb
-sudo apt-get install -f
+sudo apt-get install -y -f
 run_keybase
